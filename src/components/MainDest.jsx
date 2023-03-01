fc631d32e55ea4dfec93faaeb6104fd1834d9282
AGENT-44 <vinaymudgal441163@gmail.com>
2023-03-02 03:58:51 +0530

    sepratedComponentDist

---
+++ src/components/MainDest.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { moon, mars, europa, titan } from "../imports";
+import data from "../../data.json";
+
+const MainDest = ({ planetActive, setPlanetActive }) => {
+  return (
+    <div>
+      <div className="w-[64rem]  h-auto  flex justify-between translate-x-64 translate-y-[22rem]">
+        <div id="img">
+          <img
+            src={
+              planetActive == 0
+                ? moon
+                : planetActive == 1
+                ? mars
+                : planetActive == 2
+                ? europa
+                : titan
+            }
+            alt="moon"
+          />
+        </div>
+        <div id="text" className="text-white ">
+          <ul className="uppercase flex text-[#D0D6F9] font-thin gap-10 text-lg tracking-widest mr-14 -translate-y-10 font-barlow">
+            {data.destinations.map((item, index) => {
+              if (planetActive == index) {
+                return (
+                  <li
+                    className={"border-b-4 pb-2 text-white"}
+                    onClick={() => setPlanetActive(index)}
+                    key={index}
+                  >
+                    {item.name}
+                  </li>
+                );
+              } else {
+                return (
+                  <li
+                    className={""}
+                    onClick={() => setPlanetActive(index)}
+                    key={index}
+                  >
+                    {item.name}
+                  </li>
+                );
+              }
+            })}
+          </ul>
+          <div className="h-auto w-[29rem] ">
+            <h1 className="text-[6.5rem] font-bellefair uppercase mt-0  ">
+              {data.destinations.at(planetActive).name}
+            </h1>
+            <p className="text-left w-[27rem] tracking-[0.06em] leading-8 text-[#D0D6f9] text-base font-thin">
+              See our planet as you’ve never seen it before. A perfect relaxing
+              trip away to help regain perspective and come back refreshed.
+              While you’re there, take in some history by visiting the Luna 2
+              and Apollo 11 landing sites.
+            </p>
+            <hr className="w-auto mt-10  border-1 border-gray-600" />
+            <div className="font-barlow tracking-[0.2em] text-base text-[#D0D6f9] flex justify-between">
+              <h2 className="mt-2">AVG. DISTANCE</h2>
+              <h2 className="mt-2 mr-14">EST. TRAVEL TIME</h2>
+            </div>
+            <div className="flex justify-between text-3xl font-bellefair uppercase">
+              <p className="mt-2 ">
+                {data.destinations.at(planetActive).distance}
+              </p>
+              <p className="w-[12.5rem] mt-2  ">
+                {data.destinations.at(planetActive).travel}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainDest;
