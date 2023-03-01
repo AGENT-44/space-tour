571b42c0d765105850837c04d4cb5177c75dc47f
AGENT-44 <vinaymudgal441163@gmail.com>
2023-03-02 00:35:34 +0530

    mobile-responsive Navbar Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
   return (
     <div className="font-barlow">
-      <nav className="h-20 flex justify-between w-full top-10 bg-transparent fixed z-10 text-white">
+      <nav className="h-20 flex justify-between w-full top-5 lg:top-10 bg-transparent fixed z-10 text-white">
         <img src={Logo} alt="Logo" className="w-14 h-14 mt-3 ml-10" />
         <hr className="w-[20%] absolute border-gray-700 left-48 z-10 top-10 lg:block lg:w-[27%]  xl:w-[33%] hidden" />
         <ul className="hidden sm:flex items-center  gap-14 uppercase sm:pl-5 sm:pr-10 xl:w-[55%] lg:w-[58%] xl:pl-24 pr-36 backdrop-blur-xl bg-white/5">
@@ -94,10 +94,72 @@ const Navbar = () => {
       <div
         className={
           isOpen
-            ? "absolute sm:hidden h-full w-[60%] backdrop-blur-xl right-0 top-0 z-[2]"
+            ? "absolute sm:hidden h-full w-[60%] backdrop-blur-2xl bg-white/5 right-0 top-0 z-[2]"
             : "hidden"
         }
-      ></div>
+      >
+        <ul className=" h-96 mt-52 pl-10 flex flex-col gap-y-10">
+          <Link to={"/"}>
+            <li
+              className={
+                active == "Home"
+                  ? "font-bold text-white cursor-pointer border-r-4"
+                  : "font-bold text-white cursor-pointer"
+              }
+              onClick={() => handleChangeActive("Home")}
+            >
+              <p className={"tracking-widest text-2xl"}>
+                00 <span className=" text-white font-thin ml-2">HOME</span>
+              </p>
+            </li>
+          </Link>
+          <Link to={"/destination"}>
+            <li
+              className={
+                active == "Destination"
+                  ? "font-bold text-white cursor-pointer border-r-4"
+                  : "font-bold text-white cursor-pointer"
+              }
+              onClick={() => handleChangeActive("Destination")}
+            >
+              <p className={"tracking-widest text-2xl"}>
+                01{" "}
+                <span className=" text-white font-thin ml-2">Destination</span>
+              </p>
+            </li>
+          </Link>
+          <Link to={"/crew"}>
+            <li
+              className={
+                active == "Crew"
+                  ? "font-bold text-white cursor-pointer border-r-4"
+                  : "font-bold text-white cursor-pointer"
+              }
+              onClick={() => handleChangeActive("Crew")}
+            >
+              <p className={"tracking-widest text-2xl"}>
+                02 <span className=" text-white font-thin ml-2">Crew</span>
+              </p>
+            </li>
+          </Link>
+          <Link to={"/technology"}>
+            <li
+              className={
+                active == "Technology"
+                  ? "font-bold text-white cursor-pointer border-r-4"
+                  : "font-bold text-white cursor-pointer"
+              }
+              onClick={() => handleChangeActive("Technology")}
+            >
+              <p className={"tracking-widest text-2xl"}>
+                03{" "}
+                <span className=" text-white font-thin ml-2">Technology</span>
+              </p>
+            </li>
+          </Link>
+        </ul>
+      </div>
+      {console.log(isActive)}
     </div>
   );
 };
