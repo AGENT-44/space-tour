9b01a8633be7daa8b56f847158ac23a8ac01fbf0
AGENT-44 <vinaymudgal441163@gmail.com>
2023-03-02 03:54:49 +0530

    desktop-mode-destination Done

--- src/imports.js
+++ src/imports.js
@@ -7,7 +7,9 @@ import Destination from "./pages/Destination";
 import Logo from '../Public/shared/logo.svg'
 import iconClose from '../Public/shared/icon-close.svg';
 import iconOpen from '../Public/shared/icon-hamburger.svg';
+import moon from '../Public/destination/image-moon.png'
+import mars from '../Public/destination/image-mars.png'
+import titan from '../Public/destination/image-titan.png'
+import europa from '../Public/destination/image-europa.png'
 
-
-
-export { Navbar, Home, Destination, Router, Crew, Technology, Logo, iconClose, iconOpen }
+export { Navbar, Home, Destination, Router, Crew, Technology, moon, Logo, iconClose, iconOpen, mars, titan, europa }
