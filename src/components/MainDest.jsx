dd5e729e74c671dc91b268632c051bcbf8c4c5ad
AGENT-44 <vinaymudgal441163@gmail.com>
2023-03-03 01:09:42 +0530

    mobile-responsive-Done 1 resolution left

--- src/components/MainDest.jsx
+++ src/components/MainDest.jsx
@@ -5,10 +5,10 @@ import data from "../../data.json";
 const MainDest = ({ planetActive, setPlanetActive }) => {
   return (
     <div>
-      <div className="xl:w-[64rem] border  h-auto  lg:flex lg:justify-between xl:mx-auto xl:translate-y-[22rem] lg:w-[58rem] lg:mx-auto sm:translate-y-52 lg:translate-y-[20rem] ">
+      <div className="xl:w-[64rem]   h-auto  lg:flex lg:justify-between xl:mx-auto xl:translate-y-[22rem] lg:w-[58rem] translate-y-40 lg:mx-auto sm:translate-y-52 border border-transparent lg:translate-y-[20rem] ">
         <div
           id="img"
-          className="sm:absolute sm:top-0 lg:static sm:left-0 sm:-translate-y-10 sm:right-0 sm:mx-auto lg:-translate-y-0 sm:h-[17rem] sm:w-[17rem] lg:w-auto lg:h-auto"
+          className="sm:absolute sm:top-10 lg:static sm:left-0 sm:-translate-y-10 sm:right-0 sm:mx-auto h-[10rem] w-[10rem] mx-auto lg:-translate-y-0 sm:h-[17rem] sm:w-[17rem] lg:w-auto lg:h-auto"
         >
           <img
             src={
@@ -24,7 +24,7 @@ const MainDest = ({ planetActive, setPlanetActive }) => {
           />
         </div>
         <div id="text" className="text-white">
-          <ul className="uppercase flex text-[#D0D6F9] font-thin gap-10 lg:text-lg tracking-widest lg:mr-14 lg:-translate-y-10 font-barlow sm:mt-64 sm:justify-center lg:mt-0">
+          <ul className="uppercase flex text-[#D0D6F9] font-thin gap-10 lg:text-lg tracking-widest lg:mr-14 lg:-translate-y-10 mt-5 sm:mt-[20rem] justify-center font-barlow sm:justify-center lg:mt-0">
             {data.destinations.map((item, index) => {
               if (planetActive == index) {
                 return (
@@ -51,23 +51,37 @@ const MainDest = ({ planetActive, setPlanetActive }) => {
               }
             })}
           </ul>
-          <div className="h-auto lg:w-[29rem] sm:w-[28rem] sm:mx-auto border">
-            <h1 className="text-[6.5rem] font-bellefair uppercase mt-0 sm:text-center">
+          <div className="h-auto lg:w-[29rem] sm:w-[28rem] w-auto  sm:mx-auto ">
+            <h1 className="sm:text-[6.5rem] text-6xl text-center mt-5 font-bellefair uppercase sm:text-center">
               {data.destinations.at(planetActive).name}
             </h1>
-            <p className="lg:text-left lg:w-[27rem] sm:mt-0 sm:text-center tracking-[0.06em] leading-8 text-[#D0D6f9] text-base font-thin">
+            <p className="lg:text-left text-center p-5 sm:p-0 lg:w-[27rem] sm:mt-0 sm:text-center tracking-[0.06em] leading-0 sm:leading-8 text-[#D0D6f9] text-base sm:text-xl font-thin">
               {data.destinations.at(planetActive).description}
             </p>
-            <hr className="w-[30rem] mt-10  border-1 border-gray-600" />
-            <div className="font-barlow tracking-[0.2em] text-base text-[#D0D6f9] flex sm:justify-center lg:justify-between sm:gap-x-20 lg:gap-0">
-              <h2 className="mt-2">AVG. DISTANCE</h2>
-              <h2 className="mt-2 mr-14">EST. TRAVEL TIME</h2>
+            {/* MOBILE ABOVE FOOTER */}
+            <hr className="sm:w-[30rem] w-auto  mx-5 mt-5 sm:mt-10 border-1 sm:border-1 border-gray-600" />
+            <div className="hidden font-barlow tracking-[0.2em] text-base sm:text-xl text-[#D0D6f9] sm:flex sm:justify-center lg:justify-between sm:gap-x-20  sm:w-[30rem] lg:w-auto  lg:gap-0 ">
+              <h2 className="mt-2 sm:ml-8">AVG. DISTANCE</h2>
+              <h2 className="mt-2 sm:pr-0 pr-14">EST. TRAVEL TIME</h2>
             </div>
-            <div className="flex sm:justify-center sm:gap-x-10 border lg:justify-between text-3xl font-bellefair uppercase">
-              <p className="mt-2 border">
+            <div className="hidden sm:flex border sm:justify-center lg:gap-x-20 lg:justify-between text-3xl font-bellefair uppercase">
+              <p className="mt-2 lg:pl-0  w-56  border  lg:mr-0">
                 {data.destinations.at(planetActive).distance}
               </p>
-              <p className="w-[12.5rem] border mt-2  ">
+              <p className="lg:w-[14rem]  pl-5 border  mt-2  ">
+                {data.destinations.at(planetActive).travel}
+              </p>
+            </div>
+            {/* MOBILE FOOTER */}
+            <div className="sm:hidden text-center font-barlow tracking-[0.2em] text-base text-[#D0D6f9] flex flex-col ">
+              <h2 className="pt-2">AVG. DISTANCE</h2>
+              <h2 className="pt-2  text-white text-3xl font-bellefair uppercase">
+                {data.destinations.at(planetActive).distance}
+              </h2>
+            </div>
+            <div className="sm:hidden mt-0 flex flex-col text-center  uppercase tracking-[0.2em] text-base text-[#D0D6f9]">
+              <p className="pt-2 font-barlow">EST. TRAVEL TIME</p>
+              <p className=" pt-2  text-white text-3xl font-bellefair uppercase">
                 {data.destinations.at(planetActive).travel}
               </p>
             </div>
