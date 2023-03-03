dda616d600093c86fa72dc683e68b1249cbd1218
AGENT-44 <vinaymudgal441163@gmail.com>
2023-03-04 04:51:34 +0530

    tablet responsive

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -12,13 +12,13 @@ const Technology = () => {
   const [activeNav, setActiveNav] = useState(0);
   const navItems = [1, 2, 3];
   const ImageNavClass =
-    "h-12 w-12 py-[0.6rem] transition-all px-4 border-gray-600 rounded-full text-white border";
+    "h-12 w-12 py-[0.6rem] sm:h-14 sm:w-14 sm:py-[0.7rem] sm:px-5  transition-all px-4 border-gray-600 rounded-full text-white border";
   const ImageNavClassActive =
-    "h-12 w-12 py-[0.6rem]  transition-all bg-white px-4 border-gray-600 rounded-full text-black border";
+    "h-12 w-12 py-[0.6rem] sm:h-14 sm:w-14 sm:py-[0.7rem] sm:px-5  transition-all bg-white px-4 border-gray-600 rounded-full text-black border";
   return (
     <div className="h-screen  bg-no-repeat relative top-0 bg-cover  bg-right lg:bg-bg-technology-desktop sm:bg-bg-tech-tablet bg-bg-tech-mobile">
       <div className=" lg:h-30 lg:w-[26rem] sm:absolute sm:translate-x-0 sm:translate-y-0 sm:top-32 sm:left-12 lg:static lg:translate-x-24 xl:translate-x-48 lg:translate-y-56 absolute top-28 left-[33%]">
-        <p className="text-gray-500 text-lg lg:text-3xl sm:text-xl font-barlow tracking-[0.2em] sm:tracking-[0.3rem] font-extrabold  ">
+        <p className="text-gray-500 text-lg lg:text-3xl sm:text-2xl font-barlow tracking-[0.2em] sm:tracking-[0.3rem] font-extrabold  ">
           03
           <span className="text-white  uppercase pl-5 sm:pl-7 font-normal">
             Space lauch 101
@@ -26,9 +26,9 @@ const Technology = () => {
         </p>
       </div>
       {/* IMAGE */}
-      <div className="translate-y-44 ">
+      <div className="translate-y-44 sm:translate-y-48">
         <img
-          className="tall:h-auto tall:w-auto h-[11rem] object-fill w-full"
+          className="tall:h-auto tall:w-auto h-[11rem]  w-full sm:w-full sm:h-[18rem]"
           src={
             activeNav == 0
               ? spaceShipLandscape
@@ -39,7 +39,7 @@ const Technology = () => {
         />
       </div>
       {/* IMAGE NAVIGATION */}
-      <div className="mt-52  flex gap-5 text-center font-bellefair text-lg justify-center">
+      <div className="mt-52 sm:mt-60 flex gap-5 text-center font-bellefair text-lg sm:text-2xl justify-center">
         {navItems.map((item, index) => {
           return (
             <div
@@ -57,19 +57,19 @@ const Technology = () => {
       {/* Main */}
       <main>
         {/* Terminology */}
-        <div className="tall:mt-4 mt-10 text-center">
-          <h2 className="text-[#D0D6F9] uppercase font-barlow tracking-widest text-xl">
+        <div className="tall:mt-4 mt-5 sm:mt-10 ml-5 text-center">
+          <h2 className="text-[#D0D6F9] uppercase font-barlow tracking-widest text-xl sm:text-2xl">
             The Terminology ...
           </h2>
         </div>
         {/* Tech Name */}
         <div className="text-white  mt-2">
-          <h1 className="text-center font-bellefair uppercase text-3xl">
+          <h1 className="text-center font-bellefair uppercase text-3xl sm:text-4xl">
             {data.technology.at(activeNav).name}
           </h1>
         </div>
         {/* Description */}
-        <div className="text-[#D0D6F9] w-[95%] mx-auto text-center tall:mt-0 mt-0 tall:leading-6 text-lg tall:text-sm leading-8 p-5 tall:p-2">
+        <div className="text-[#D0D6F9] w-[95%] mx-auto text-center tall:mt-0 mt-0 tall:leading-6 text-lg tall:text-sm leading-8 p-5 tall:p-2 sm:pt-2 sm:text-xl  sm:w-[70%] ">
           <p>{data.technology.at(activeNav).description}</p>
         </div>
       </main>
